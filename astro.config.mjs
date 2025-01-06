557067cfed3db5b94af22727f2cc787c7fa6fcc0
Quentin Ropele <quentin.ropele@hotmail.fr>
2025-01-06 22:19:09 +0100

    try

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,5 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: 'https://nagazx.github.io',
-  base: 'docs',
+  base: 'mariage',
 });
